6d671c2eb666d911984a6042deb8d726e233763e
dryans1 <dryans@naver.com>
2020-08-24 23:50:49 +0900

    Edit D1 files

--- D1/2056.py
+++ D1/2056.py
@@ -18,26 +18,3 @@ for tc in range(1, T+1):
     print(f'#{tc} {result}')
 
 
-
-
-days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-T = int(input())
-for tc in range(1, T+1):
-    temp = input()
-
-    year = temp[:4]
-    month = temp[4:6]
-    day = temp[6:]
-
-    
-    flag = True
-
-    if int(month) <= 0 or int(month) >= 13:
-        flag = False
-    elif int(day) > days[int(month)] or int(day) <= 0:
-        flag = False
-    
-    if flag:
-        print("#{} {}/{}/{}" .format(tc, year, moth, day))
-    else:
-        print("#{} -1" .format(tc))
